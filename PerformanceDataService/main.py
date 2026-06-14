c9a167f59f2122473063265c85918166dee1ceb6
Halbolonen <halbolonen@gmail.com>
2026-06-14 20:11:13 +0100

    added usage guide for the pd service
    added usage guide for the pd service

--- PerformanceDataService/main.py
+++ PerformanceDataService/main.py
@@ -23,4 +23,15 @@ def remove_items(removal_list: list[str]) -> list[str]:
         while item in items:
             items.remove(item)
     return items
-    
+
+
+# use Invoke-RestMethod to call these things from PS.
+    """
+        example commands:
+        PS C:\WINDOWS\system32> Invoke-RestMethod -Method 'Post' -Uri http://localhost:8000/remove -ContentType "application/json" -Body '["B738","A320"]'
+        PS C:\WINDOWS\system32> Invoke-RestMethod -Method 'Post' -Uri http://localhost:8000/items?item=B752
+
+    """
+
+# use uvicorn main:app --reload to run the API endpoint. make sure you are in the directory of main and in the venv when you do that.
+# enter venv using .\venv\Scripts\activate
